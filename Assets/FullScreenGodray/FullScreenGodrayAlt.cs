8019d3b7fb4f4e4f479edf81d365baa24529f472
herbchow <HerbertC@iqmetrix.com>
2014-06-30 15:04:30 -0700

    Add extra camera for rendering light source full screen quad

--- Assets/FullScreenGodray/FullScreenGodrayAlt.cs
+++ Assets/FullScreenGodray/FullScreenGodrayAlt.cs
@@ -4,9 +4,26 @@ using System.Collections;
 public class FullScreenGodrayAlt : MonoBehaviour
 {
     public Shader ItemMaskShader;
-    private RenderTexture MaskRt;
+    public Shader LightSourceShader;
+    public Texture2D LightSource;
+
+    public RenderTexture MaskRt;
+    public RenderTexture LightSourceRt;
     private GameObject _godrayCamera;
 
+    private Material _lightSourceMaterial;
+
+    private Material LightSourceMaterial
+    {
+        get
+        {
+            if (_lightSourceMaterial == null)
+            {
+                _lightSourceMaterial = new Material(LightSourceShader);
+            }
+            return _lightSourceMaterial;
+        }
+    }
     private GameObject GodrayCamera
     {
         get
@@ -21,12 +38,14 @@ public class FullScreenGodrayAlt : MonoBehaviour
         }
     }
 
+
     // Use this for initialization
     private void Start()
     {
         var rtWidth = Screen.width;
         var rtHeight = Screen.height;
         MaskRt = new RenderTexture(rtWidth, rtHeight, 16);
+        LightSourceRt = new RenderTexture(rtWidth, rtHeight, 16);
     }
 
     private void OnPreRender()
@@ -36,10 +55,29 @@ public class FullScreenGodrayAlt : MonoBehaviour
         cam.clearFlags = CameraClearFlags.SolidColor;
         cam.backgroundColor = Color.red;
         cam.RenderWithShader(ItemMaskShader, "RenderType");
+        //RenderLightSource();
+        // Draw a quad with material
+        //RenderLightSource(LightSourceMaterial, LightSourceRt);
     }
 
-    private void OnRenderImage(RenderTexture source, RenderTexture destination)
-    {
-        Graphics.Blit(MaskRt, destination);
-    }
+    //private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    //{
+    //    // TODO: The src will always be the stuff rendered on The Main.Camera (where this script is attached to)
+    //    RenderLightSource(src);
+    //}
+
+    //private void RenderLightSource(RenderTexture src)
+    //{
+    //    //var saved = RenderTexture.active;
+    //    LightSourceMaterial.SetTexture("_LightSourceTex", LightSource);
+    //    LightSourceMaterial.SetVector("_LightScreenPos", new Vector4(0.18f, 0.48f, 0, 0));
+    //    LightSourceMaterial.SetFloat("_LightSize", 0.33f);
+    //    ImageEffects.BlitWithMaterial(LightSourceMaterial, MaskRt, LightSourceRt);
+    //    //RenderTexture.active = saved;
+    //}
+
+    //private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    //{
+    //    Graphics.Blit(LightSourceRt, destination);
+    //}
 }
--- Assets/FullScreenGodray/LightSource.shader
+++ Assets/FullScreenGodray/LightSource.shader
@@ -1,26 +1,52 @@
 ﻿Shader "Custom/LightSource" {
-	Properties {
-		_MainTex ("Base (RGB)", 2D) = "white" {}
-	}
+Properties {
+		_LightSourceTex ("Light Source", 2D) = "white" {}
+		_LightScreenPos ("Light Screen Position", Vector) = (0,0,0,0)
+		_UvOffset ("UV Offset", Vector) = (0,0,0,0)
+		_LightSize ("Light size", Float) = 2.0
+			}
 	SubShader {
-		Tags { "RenderType"="Opaque" }
-		LOD 200
-		
-		CGPROGRAM
-		#pragma surface surf Lambert
+            Tags { "Queue" = "Transparent" }
+            Pass {
 
-		sampler2D _MainTex;
+			    CGPROGRAM
+			    #pragma vertex vert
+			    #pragma fragment frag
 
-		struct Input {
-			float2 uv_MainTex;
-		};
+			    #include "UnityCG.cginc"
 
-		void surf (Input IN, inout SurfaceOutput o) {
-			half4 c = tex2D (_MainTex, IN.uv_MainTex);
-			o.Albedo = c.rgb;
-			o.Alpha = c.a;
+    			uniform float2 _LightScreenPos;
+                uniform float2 _UvOffset;
+    			uniform sampler2D _MainTex;
+				uniform sampler2D _LightSourceTex;
+                uniform float _LightSize;
+
+			    struct v2f {
+			    	float4 pos : POSITION;
+			    	float2 uv : TEXCOORD0;
+			    };
+
+			    v2f vert( appdata_img v )
+			    {
+			    	v2f o;
+			    	o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+			    	o.uv = v.texcoord;
+			    	return o;
+			    }
+
+			    float4 frag(v2f i) : COLOR
+			    {
+                    half4 color = half4(0,0,0,1);
+                    // Find light position in world and map it to screen space
+                    float2 coord = 0.5f - (i.uv - _LightScreenPos) / _LightSize * 0.5f;
+                    color += pow(tex2D(_LightSourceTex,coord),2) * 2;
+					return color;
+			    }
+
+			    ENDCG
 		}
-		ENDCG
+
+
 	} 
 	FallBack "Diffuse"
 }
